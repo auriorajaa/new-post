5eba5ebb2a29c3db408acf94e8286001cdde427d
Aurio Rajaa <riorajaa2018@gmail.com>
2026-09-08 17:27:36 +0700

    create jwt for user login

--- apps/api/src/auth/auth.resolver.ts
+++ apps/api/src/auth/auth.resolver.ts
@@ -1,15 +1,16 @@
 import { Args, Mutation, Resolver } from "@nestjs/graphql";
 import { AuthService } from "./auth.service.js";
 import { SignInInput } from "./dto/signin.input.js";
+import { AuthPayload } from "./entities/auth-payload.entity.js";
 
 @Resolver()
 export class AuthResolver {
   constructor(private readonly authService: AuthService) {}
 
-  @Mutation(() => String)
+  @Mutation(() => AuthPayload)
   async signIn(@Args("signInInput") signInInput: SignInInput) {
     const user = await this.authService.validateLocalUser(signInInput);
 
-    return "Login successfully";
+    return await this.authService.login(user);
   }
 }
