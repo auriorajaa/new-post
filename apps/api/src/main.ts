816c5fcdf02e72e4f3d4a255a60482ad69312b72
Aurio Rajaa <riorajaa2018@gmail.com>
2026-09-08 16:20:52 +0700

    create auth module

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,11 +1,13 @@
 import "dotenv/config";
 import { NestFactory } from "@nestjs/core";
 import { AppModule, ObserveInstrument } from "./app.module.js";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     instrument: ObserveInstrument,
   });
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 8080);
 }
 await bootstrap();
