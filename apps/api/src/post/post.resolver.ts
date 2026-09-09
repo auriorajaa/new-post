92a74b51ca4ebdf391965e416a3ffb114dc50618
Aurio Rajaa <riorajaa2018@gmail.com>
2026-09-09 11:25:08 +0700

    add user auth guard

--- apps/api/src/post/post.resolver.ts
+++ apps/api/src/post/post.resolver.ts
@@ -1,13 +1,18 @@
-import { Resolver, Query } from "@nestjs/graphql";
+import { Resolver, Query, Context } from "@nestjs/graphql";
 import { PostService } from "./post.service.js";
 import { Post } from "./entities/post.entity.js";
+import { UseGuards } from "@nestjs/common";
+import { JwtAuthGuard } from "../auth/guards/jwt-auth/jwt-auth.guard.js";
 
 @Resolver(() => Post)
 export class PostResolver {
   constructor(private readonly postService: PostService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Query(() => [Post], { name: "posts" })
-  findAll() {
+  findAll(@Context() context: any) {
+    const user = context.req.user;
+    console.log({ user });
     return this.postService.findAll();
   }
 }
