1c1f44ccefc805fe1d5c4f3dffe05d26bcc17cb2
Aurio Rajaa <riorajaa2018@gmail.com>
2026-09-17 14:09:15 +0700

    fix require module error on next js

--- apps/front/next.config.ts
+++ apps/front/next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
 const nextConfig: NextConfig = {
+  serverExternalPackages: ["jsdom", "html-encoding-sniffer"],
   images: {
     remotePatterns: [
       {
